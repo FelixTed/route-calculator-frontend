c82766e50bdcc87443eed1560c8bad3f80243e97
Felix Cadieux <cadieuxfelix@outlook.com>
2025-05-08 19:35:03 -0400

    Can now fetch info from api

--- RouteCalculator/RouteCalculator/src/app/map-view/map-view.component.ts
+++ RouteCalculator/RouteCalculator/src/app/map-view/map-view.component.ts
@@ -1,16 +1,22 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component, DestroyRef, inject, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-
-
 @Component({
   selector: 'app-map-view',
   imports: [],
   templateUrl: './map-view.component.html',
   styleUrl: './map-view.component.css'
 })
-export class MapViewComponent implements AfterViewInit {
+export class MapViewComponent implements AfterViewInit, OnInit {
+
+  private httpClient = inject(HttpClient)
+  private destroyRef = inject(DestroyRef)
 
   private map:any;
+  private routes:string = ''
+  
+  private apiURL = "http://localhost:8080/api/routes/45.509062/-73.553363/10000"
+
 
   private initMap(): void {
     this.map = L.map('map', {
@@ -32,5 +38,14 @@ export class MapViewComponent implements AfterViewInit {
     this.initMap()
   }
 
-   
+  ngOnInit(): void {
+    const subscription = this.httpClient.get<string>(this.apiURL).subscribe(routes => {
+      this.routes = routes
+      console.log(this.routes)
+    })
+    console.log(this.routes)
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe()
+    })
+  }
 }
